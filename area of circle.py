ef30cab0fc2645dc92694d40e8cccb7261df098e
Ujwala Udaykumar Mane <93083628+Ujwalamane12@users.noreply.github.com>
2023-04-16 22:30:15 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+
+radius = float(input("Input the radius of the circle: "))
+area = math.pi * radius ** 2
+
+print(f"The area of the circle with radius {radius} is: {area}")
+
+
+# In[ ]:
+
+
+
+
---
+++ filename.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+filename = input("Input the Filename: ")
+file_extension = filename.split(".")[-1]
+if(file_extension=='py'):
+     print("The extension of the file is :python")
+elif(file_extension=='c'):
+     print("The extension of the file is :c programming")
+elif(file_extension=='java'):
+     print("The extension of the file is :java")
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
